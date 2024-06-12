17237495f64686871d51b5003ec5d6460ca56e58
Renmy <renmyenriquez@gmail.com>
2024-06-11 21:01:32 -0400

    fixed Models to match intructions

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -1,21 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: {type:String, unique:true},
-    phone: String,
-    linkedinUrl: String,
-    languages: Array,
-    program: String,
-    background: String,
-    image: String,
-    cohort_id: Number,
-    projects: Array
-  })
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  phone: { type: String, required: true },
+  linkedinUrl: { type: String, default: "" },
+  languages: {
+    type: [String],
+    enum: [
+      "English",
+      "Spanish",
+      "French",
+      "German",
+      "Portuguese",
+      "Dutch",
+      "Other",
+    ],
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  background: { type: String, default: "" },
+  image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
+  cohort: Schema.ObjectId,
+  projects: Array,
+});
 
-  const Student = mongoose.model('Student', studentSchema);
+const Student = mongoose.model("Student", studentSchema);
 
-
-  module.exports = Student;
+module.exports = Student;
